f93e1331a20dec892efd792136b3c66c183e410d
Magdalena Kuśmierczak <magdalena-jarek@o2.pl>
2024-06-03 16:32:57 +0200

    Update EmployeeInfo.js
    secound func

--- src/EmployeeInfo.js
+++ src/EmployeeInfo.js
@@ -36,65 +36,87 @@ const employee = [
 //To call nested data e.g. firstName of manager
 // call getEmployeeInfo(employee, "manager.firstName")
 
-// const getEmployeeInfo = (employee, key) => {
-//   //Calling data
-//   const personalInformation = Object.values(employee[0].personalInfo);
-//   const employmentDetailsList = Object.values(employee[1].employmentDetails);
+const getEmployeeInfo = (employee, key) => {
+  //Calling data
+  const personalInformation = Object.values(employee[0].personalInfo);
+  const employmentDetailsList = Object.values(employee[1].employmentDetails);
 
-//   //Function to get keys from object in array
-//   const dataKeys = (arr) => {
-//     let keysArray = [];
-//     for (let value of Object.values(arr)) {
-//       const dataKey = Object.keys(value);
-//       keysArray = [...keysArray, ...dataKey];
-//     }
-//     return keysArray;
-//   };
+  //Function to get keys from object in array
+  const dataKeys = (arr) => {
+    let keysArray = [];
+    for (let value of Object.values(arr)) {
+      const dataKey = Object.keys(value);
+      keysArray = [...keysArray, ...dataKey];
+    }
+    return keysArray;
+  };
 
-//   //Calling data with validation
-//   if (dataKeys(personalInformation).includes(key)) {
-//     const personalInfoData = employee[0].personalInfo.find((data) => data[key]);
-//     return console.log(personalInfoData);
-//   }
-//   if (dataKeys(employmentDetailsList).includes(key)) {
-//     const employmentData = employee[1].employmentDetails.find(
-//       (data) => data[key]
-//     );
-//     return console.log(employmentData);
-//   }
+  //Calling data with validation
+  if (dataKeys(personalInformation).includes(key)) {
+    const personalInfoData = employee[0].personalInfo.find((data) => data[key]);
+    return console.log(personalInfoData);
+  }
+  if (dataKeys(employmentDetailsList).includes(key)) {
+    const employmentData = employee[1].employmentDetails.find(
+      (data) => data[key]
+    );
+    return console.log(employmentData);
+  }
 
-//   // Calling nested data
-//   //Address data
-//   if (key.startsWith("address.")) {
-//     const addressData = Object.values(personalInformation[3].address);
-//     const addressKey = key.split(".")[1];
-//     if (dataKeys(addressData).includes(addressKey)) {
-//       const addressInfo = addressData.find((data) => data[addressKey]);
-//     return console.log(addressInfo);
-//     }
-//   }
-//   //Manager data
-//   if (key.startsWith("manager.")) {
-//     const managerData = Object.values(employmentDetailsList[4].manager);
-//     const managerKey = key.split(".")[1];
-//     //Key validation
-//     if (dataKeys(managerData).includes(managerKey)) {
-//       const managerInfo = managerData.find((data) => data[managerKey]);
-//       return console.log(managerInfo);
-//     }
-//   }
+  // Calling nested data
+  //Address data
+  if (key.startsWith("address.")) {
+    const addressData = Object.values(personalInformation[3].address);
+    const addressKey = key.split(".")[1];
+    if (dataKeys(addressData).includes(addressKey)) {
+      const addressInfo = addressData.find((data) => data[addressKey]);
+      return console.log(addressInfo);
+    }
+  }
+  //Manager data
+  if (key.startsWith("manager.")) {
+    const managerData = Object.values(employmentDetailsList[4].manager);
+    const managerKey = key.split(".")[1];
+    //Key validation
+    if (dataKeys(managerData).includes(managerKey)) {
+      const managerInfo = managerData.find((data) => data[managerKey]);
+      return console.log(managerInfo);
+    }
+  }
 
-//   return console.log("Klucz nie istnieje");
-// };
+  return console.log("Klucz nie istnieje");
+};
 
-const getEmployeeInfo = (employee) => {
+//To call data from database 'employee' with certain key e.g. "firstName",
+// call getEmployeeInfo2(employee, "firstName")
+//To call nested data e.g. firstName of manager
+// call getEmployeeInfo(employee, "manager.firstName")
+
+const getEmployeeInfo2 = (employee, key) => {
+  const nestedKeys = key.split(".");
+  let keyFound = false;
   for (const typeOfInformation of Object.values(employee)) {
-    for (const informations of Object.entries(Object.values(typeOfInformation))) {
-      console.log(informations);
+    for (const informations of Object.entries(typeOfInformation)) {
       for (const information of Object.values(informations[1])) {
-        console.log(information);
+        for (const [infoKey, value] of Object.entries(information)) {
+          if (infoKey === key) {
+            keyFound = true;
+            return console.log(value);
+          }
+          //Calling nested data
+          if (infoKey === nestedKeys[0]) {
+            for (const nestedInfo of Object.values(value)) {
+              if (Object.keys(nestedInfo).includes(nestedKeys[1])) {
+                keyFound = true;
+                return console.log(nestedInfo[nestedKeys[1]]);
+              }
+            }
+          }
+        }
       }
     }
   }
+  if (!keyFound) {
+    console.log("Klucz nie istnieje");
+  }
 };
-getEmployeeInfo(employee);
